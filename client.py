398146c45181350b88f3a300b70cb18e228c1145
Isaac Merchan Blanco <imerchan@f-l3209-pc24.aulas.etsit.urjc.es>
2019-11-18 10:39:26 +0100

    clientecasi

--- client.py
+++ client.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
+# Constantes. Dirección IP del servidor,Puerto del servidor y
+# Linea
+try:
+    METODO = sys.argv[1]
+    if METODO.upper() != ('INVITE' or 'BYE'):
+        print()
+        sys.exit("Method must be INVITE or BYE")
+    LOGIN_IP = sys.argv[2].split(':')[0] #login@ip (me estoy quedando con todo junto)
+    IPSERVER = LOGIN.split('@')[1]#la necesito para concetarme solamente
+    PORTSERVER = int(sys.argv[2].split(':')[1])
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+except ValueError:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
+except IndexError:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
-
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.connect((IPSERVER, PORTSERVER))
+    print("Enviando:", METODO.upper() + ' sip:' + LOGIN_IP + ' SIP/2.0')
+    # Se lo enviamos al servidor
+    my_socket.send(bytes(METODO.upper() + ' sip:' + LOGIN_IP + ' SIP/2.0\r\n'
+                            + b'\r\n\r\n'))
     data = my_socket.recv(1024)
-
+    recibido = data.decode('utf-8')
     print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
 
-print("Fin.")
+    if METODO.upper() == 'INVITE':
+         if recibidob[2] == 'Trying' and recb[5] == "Ringing" and recb[8] == "OK":
+            my_socket.send(bytes('ACK sip:' + ADRESS + ' SIP/2.0', 'utf-8')
+                           + b'\r\n\r\n')
+    if METODO == 'BYE':
+
+
+print("Socket terminado.")
